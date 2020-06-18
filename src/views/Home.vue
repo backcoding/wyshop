2cb87998bd1cdde2ffc9660ae3fbd50f5c1d5015
backcoding <382418238@qq.com>
2020-06-18 11:06:24 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <van-search v-model="value" placeholder="请输入搜索关键词" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {}
+}
+</script>
